d1505e11a2adbda54250bb8efb8b876d9fc12187
echizen0312 <sakuya0312@qq.com>
2019-06-06 10:40:30 +0800

    Update test.java

--- java/test.java
+++ java/test.java
@@ -1,12 +1,15 @@
-import io.plactal.eoscommander.rpc.utils.Rpc;
+import io.plactal.eoscommander.rpc.Ecc;
+import io.plactal.eoscommander.rpc.Rpc;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Test {
     public static void main(String[] arg) {
+        // 初始化
         Rpc rpc = new Rpc("节点地址");
 
+        // 发起交易
         Map<String, Object> args = new HashMap<String, Object>();
         args.put("from", "转出账户");
         args.put("to", "转入账户");
@@ -20,5 +23,12 @@ public class Test {
                 args);
 
         System.out.println(res);
+
+        // 生成秘钥对
+        String privateKey = Ecc.seedPrivate("随机种子");
+        String publicKey = Ecc.getPublicKey(privateKey);
+
+        System.out.println(privateKey);
+        System.out.println(publicKey);
     }
 }
